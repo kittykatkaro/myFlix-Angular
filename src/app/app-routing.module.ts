74851c770ee0b8df40b45767c1d8da9b3a9f5106
Karo <karoogorek@gmail.com>
2024-12-13 15:56:10 +0100

    Add routing for profile view component and set up redirection to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
+// import { HomeComponent } from './home/home.component';
+// import { MoviesComponent } from './movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileViewComponent },
+  // { path: 'movies', component: MoviesComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

e737c8532cb124ae28326ae8cecf461f296f1641
Karo <karoogorek@gmail.com>
2024-12-13 11:11:26 +0100

    Add MovieCardComponent to app module declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,14 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
+    MovieCardComponent,
   ],
   imports: [
     BrowserModule,

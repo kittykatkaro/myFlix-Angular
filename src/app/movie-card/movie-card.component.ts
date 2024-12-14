11488d2485036df9b79eab1df939c8158100d42c
Karo <karoogorek@gmail.com>
2024-12-14 13:31:15 +0100

    Implement dialog functionality for movie details in movie card component

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { GetAllMoviesService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MovieDialogComponent } from '../movie-dialog/movie-dialog.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -10,7 +12,10 @@ import { GetAllMoviesService } from '../fetch-api-data.service';
 })
 export class MovieCardComponent {
   movies: any[] = [];
-  constructor(public fetchApiData: GetAllMoviesService) {}
+  constructor(
+    public fetchApiData: GetAllMoviesService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.getMovies();
@@ -20,7 +25,26 @@ export class MovieCardComponent {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
       console.log(this.movies);
-      return this.movies;
+      // return this.movies;
     });
+  }
+
+  // Function to open the dialog when the movie image is clicked
+  openDialog(type: string, data: any): void {
+    console.log('Dialog Type:', type); // Debugging: Check the type
+    console.log('Dialog Data:', data); // Debugging: Check the data being passed
+    this.dialog.open(MovieDialogComponent, {
+      data: { type, data },
+      width: '400px',
+    });
+  }
+
+  // dummy function to add movie to favorites (to be linked)
+  toggleFavorite(movieId: string): void {
+    console.log('Added to favorites');
+  }
+  //check if movie is in favorites (to be linked)#
+  isFavorite(movieId: string): boolean {
+    return false;
   }
 }

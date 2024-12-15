1e758b4b8b6db411055e9dbe87d6e4e2d6c2987b
Karo <karoogorek@gmail.com>
2024-12-15 11:58:57 +0100

    Remove commented-out routes for home and movies in app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileViewComponent } from './profile-view/profile-view.component';
-// import { HomeComponent } from './home/home.component';
-// import { MoviesComponent } from './movies/movies.component';
 
 const routes: Routes = [
-  // { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileViewComponent },
-  // { path: 'movies', component: MoviesComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 

d2a349bea04ad4c7d96179681e82c1ded12f2078
Karo <karoogorek@gmail.com>
2024-12-17 16:12:53 +0100

    Add JSDoc comments to MovieDialogComponent to document its purpose and properties

--- src/app/movie-dialog/movie-dialog.component.ts
+++ src/app/movie-dialog/movie-dialog.component.ts
@@ -8,6 +8,12 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   templateUrl: './movie-dialog.component.html',
   styleUrl: './movie-dialog.component.scss',
 })
+
+/**
+ * Component for the movie dialog, which displays movie details.
+ * It receives the type and data of the movie to display.
+ * The type determines the dialog title and the data is the movie details.
+ */
 export class MovieDialogComponent {
   type: string | undefined;
   data: any;
